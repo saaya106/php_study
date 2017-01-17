585cf04e5683b8d390bfb84e8f0a83779d4dc876
Saaya Takai <saaya@dh171156.pu-toyama.ac.jp>
2017-01-17 15:48:31 +0900

    フォームを作成

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-index.php
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PHP study</title>
+</head>
+<body>
+<form action="" method="GET">
+  <input type="text" name="val">
+  <button type="submit" name="submit" value="submit">送信</button>
+</form>
+</body>
+</html>
